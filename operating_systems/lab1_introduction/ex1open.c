c4ccbee148d85efbbe21559a8bfff2b58536f5b7
Paweł Drożański <pawdro@gmail.com>
2012-10-13 12:59:22 +0200

    ex 3.5.1

--- operating_systems/lab1_introduction/ex1open.c
+++ operating_systems/lab1_introduction/ex1open.c
@@ -7,12 +7,18 @@
 int main (int argc, char **argv) {
     int f1, c;
     char b[BUFSIZE], *n1;
+    int open_return, read_return;
  
     c = 10;
     n1 = argv[1];
  
     f1 = open (n1, O_RDONLY);
-    read (f1, b, c);
+    
+    open_return = f1;
+    read_return = read (f1, b, c);
+    printf("Open zwrocil: %d , read zwrocil: %d\n", open_return, read_return);
+
+
     printf("%s: Przeczytano %d znakow z pliku %s: \"%s\"\n",
 	   argv[0], c, n1, b);
     close(f1);
