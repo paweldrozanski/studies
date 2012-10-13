ce7065c9b90a297559c4ab601ca37f45554bd63d
Paweł Drożański <pawdro@gmail.com>
2012-10-13 13:08:56 +0200

    ex 3.5.2

--- operating_systems/lab1_introduction/ex1open.c
+++ operating_systems/lab1_introduction/ex1open.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
  
 #define BUFSIZE 1024
  
@@ -15,9 +16,19 @@ int main (int argc, char **argv) {
     f1 = open (n1, O_RDONLY);
     
     open_return = f1;
+    if (open_return == -1){
+      perror("Blad otwarcia");
+    }
+    else {
+      printf("Open zakonczyl sie bez bledow\n");
+    }
     read_return = read (f1, b, c);
-    printf("Open zwrocil: %d , read zwrocil: %d\n", open_return, read_return);
-
+    if (read_return == -1){
+      perror("Blad odczytu");
+    }
+    else {
+      printf("Read zakonczyl sie bez bledow\n");
+    }
 
     printf("%s: Przeczytano %d znakow z pliku %s: \"%s\"\n",
 	   argv[0], c, n1, b);
