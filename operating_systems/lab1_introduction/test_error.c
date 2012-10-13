bdd084d84dd4446c295551c8425756548d410bb0
Paweł Drożański <pawdro@gmail.com>
2012-10-13 12:52:59 +0200

    ex1open

---
+++ operating_systems/lab1_introduction/test_error.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <errno.h>
+
+int main()
+{
+  int s;
+
+  s = socket(PF_INET, SOCK_STREAM, 0);
+
+  if (s == -1) { // some error has occurred
+        // prints "socket error: " + the error message:
+             perror("socket error");
+  }
+        //
+        //     // similarly:
+  if (listen(s, 10) == -1) {
+        //         // this prints "an error: " + the error message from errno:
+  printf("an error: %s\n", strerror(errno));
+  }
+}
