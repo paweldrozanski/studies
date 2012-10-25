bc266b3f79832eb4ce2e52d11cc3f220bab215c8
Paweł Drożański <dr0p@T420.(none)>
2012-10-25 22:52:09 +0200

    ex 1.10

--- operating_systems/lab3_io_files_advanced/stat_info.c
+++ operating_systems/lab3_io_files_advanced/stat_info.c
@@ -19,6 +19,8 @@ static void print_laststch(const struct stat *sb);
 static void print_lastacc(const struct stat *sb);
 static void print_lastmod(const struct stat *sb);
 static void print_name(const struct stat *sb, char *name);
+static void print_content(char *name);
+
 /* -------------------------------------------------------------------------------- */
 
 int  main(int argc, char *argv[])
@@ -30,7 +32,7 @@ int  main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
   
-  if (stat(argv[1], &sb) == -1) {
+  if (lstat(argv[1], &sb) == -1) {
     perror("stat");
     exit(EXIT_SUCCESS);
   }
@@ -45,6 +47,7 @@ int  main(int argc, char *argv[])
   print_laststch(&sb);
   print_lastacc(&sb);
   print_lastmod(&sb);
+  print_content(argv[1]);
   
   exit(EXIT_SUCCESS);
 }
@@ -53,11 +56,11 @@ int  main(int argc, char *argv[])
 static void print_type(struct stat *sb){
   printf("File type:                ");
   switch (sb->st_mode & S_IFMT) {
+  case S_IFLNK:  printf("symlink\n");                 break;
   case S_IFBLK:  printf("block device\n");            break;
   case S_IFCHR:  printf("character device\n");        break;
   case S_IFDIR:  printf("directory\n");               break;
   case S_IFIFO:  printf("FIFO/pipe\n");               break;
-  case S_IFLNK:  printf("symlink\n");                 break;
   case S_IFREG:  printf("regular file\n");            break;
   case S_IFSOCK: printf("socket\n");                  break;
   default:       printf("unknown?\n");                break;
@@ -76,6 +79,7 @@ static void print_perms(const struct stat *sb, const char *path){
   char* execute= malloc(3*sizeof(char));
 
   printf("Mode:                     %lo \n", (unsigned long) sb->st_mode & 0700);
+  printf("Mask:                     %lo \n", (unsigned long) sb->st_mode & S_IRWXU);
 
   read = access(path, R_OK) == 0 ? "yes" : "no";
   write = access(path, W_OK) == 0 ? "yes" : "no";
@@ -106,37 +110,86 @@ void check_and_print_size(long point){
 static void print_size(const struct stat *sb){
   
 
-  printf("Preferred I/O block size:\t"); 
+  printf("Preferred I/O block size:\t"); check_and_print_size((long)sb->st_blksize);
+
   /*
   if ((long)sb->st_blksize <1024) printf("%ld B\n", (long) sb->st_blksize);
   else if ((long)sb->st_blksize < 1024*1024) printf("%ld KB\n", (long) sb->st_blksize/(1024));
   else if ((long)sb->st_blksize < 1024*1024*1024) printf("%ld MB\n", (long) sb->st_blksize/(1024));
   else printf("%ld BrontoBytes\n", (long)sb->st_blksize/(1024*1024*1024));
   */
-  check_and_print_size((long)sb->st_blksize);
-  /*printf("File size:                %lld bytes\n",(long long) sb->st_size);*/
+    /*printf("File size:                %lld bytes\n",(long long) sb->st_size);*/
   printf("File size:\t"); check_and_print_size((long long) sb->st_size);
   printf("Blocks allocated:         %lld\n",(long long) sb->st_blocks);
 }
 /* -------------------------------------------------------------------------------- */
 
+static void diff_time(time_t some_time){
+
+  time_t now;
+  time(&now);
+  int dif = difftime(now, some_time);
+  if (dif < 60) printf("%ds ago\n", dif);
+  else if (dif < 60*60) printf("%dmin and %ds ago\n", dif/60, dif %60);
+  else if (dif < 60*60*60) printf("%dh %dm and %ds ago\n", dif/3600, (dif%3600)/60, dif%(3600*60));
+  else printf("%d\n", dif);
+}
+
 static void print_laststch(const struct stat *sb){
-  printf("Last status change:       %s", ctime(&sb->st_ctime));
+  printf("\nLast status change:       %s", ctime(&sb->st_ctime));
+  printf("Last status change: %dyear %dhours %dminutes %dseconds ago.\n", gmtime(&sb->st_ctime)->tm_year, gmtime(&sb->st_ctime)->tm_hour, 
+      gmtime(&sb->st_ctime)->tm_min, gmtime(&sb->st_ctime)->tm_sec);
+  printf("Last status change: ");
+  diff_time(sb->st_ctime);
+  printf("\n");
 }
 /* -------------------------------------------------------------------------------- */
 
 static void print_lastacc(const struct stat *sb){
   printf("Last file access:         %s", ctime(&sb->st_atime));
+  printf("Last file access: %dhours %dminutes %dseconds ago.\n", gmtime(&sb->st_atime)->tm_hour, 
+      gmtime(&sb->st_atime)->tm_min, gmtime(&sb->st_atime)->tm_sec);
+  printf("Last file access: ");
+  diff_time(sb->st_atime);
+  printf("\n");
 }
 /* -------------------------------------------------------------------------------- */
 
 static void print_lastmod(const struct stat *sb){
   printf("Last file modification:   %s", ctime(&sb->st_mtime));
+  printf("Last file modification: %dhours %dminutes %dseconds ago.\n", gmtime(&sb->st_mtime)->tm_hour, 
+      gmtime(&sb->st_mtime)->tm_min, gmtime(&sb->st_mtime)->tm_sec);
+  printf("Last file modification: ");
+  diff_time(sb->st_mtime);
+  printf("\n");
 }
 /* -------------------------------------------------------------------------------- */
 
 static void print_name(const struct stat *sb, char *name){
+
   char* bname = basename(name);
   printf("Name of the file:         %s\n", bname);
+
+  if ((sb->st_mode & S_IFMT) == S_IFLNK) {
+    char *linkname;
+    ssize_t r;
+    linkname = malloc(sb->st_size + 1);
+    r = readlink(name, linkname, sb->st_size + 1);
+    linkname[sb->st_size] = '\0';
+    printf("'%s' points to '%s'\n", name, linkname);
+  }
+
 }
 /* -------------------------------------------------------------------------------- */
+static void print_content(char *name){
+  
+  int fd;
+  char ans;
+  printf("Print the argument content? ");
+  ans = getc(stdin);
+
+  if (ans == "y") {
+    fd = open(argv[1], O_RDONLY);
+  }
+
+}
