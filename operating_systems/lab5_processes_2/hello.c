bc83aac1c86a388fd1bd5676a5ca6d2ed75cbc5b
Paweł Drożański <pawdro@gmail.com>
2012-11-02 22:17:21 +0100

    ex 2.2

--- operating_systems/lab5_processes_2/hello.c
+++ operating_systems/lab5_processes_2/hello.c
@@ -18,14 +18,29 @@ void *PrintOwnNumber(void *arg){
 int main(int argc, char *argv[]){
   int pthread_array[10], i; 
   pthread_t thread[10];
+
+  pthread_attr_t attr;
+
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
   for (i=0; i<10; i++){
-    pthread_array[i] = pthread_create(&thread[i], NULL, PrintOwnNumber,(void *) i);
+    pthread_array[i] = pthread_create(&thread[i], &attr, PrintOwnNumber,(void *) i);
     if (pthread_array[i]){
       printf("Return code: %d\n", pthread_array[i]);
       exit(-1);
     }
-    sleep(1);
+
+    /* Wait for the thread */
+    pthread_array[i] = pthread_join(thread[i], NULL);
+    if (pthread_array[i]) {
+      printf("ERROR; return code from pthread_join() is %d\n", pthread_array[i]);
+      exit(-1);
+    }
   }
+
+  pthread_attr_destroy(&attr);
+  pthread_exit(NULL);
 
 return 0;
 }
